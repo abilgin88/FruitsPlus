80326cb3588658a13b73a606b5801e8ddfb462c2
Abdullah Bilgin <abilgin02@icloud.com>
2023-07-26 08:38:51 +0300

    Project Setup: Setting and grafik material (Icon, Colors, Photos etc.)

---
+++ FruitsPlus/FruitsPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsPlusApp.swift
+//  FruitsPlus
+//
+//  Created by Abdullah Bilgin on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
